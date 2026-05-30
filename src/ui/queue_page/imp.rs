Userwithaname/mellow

commit d6fc74f94f8a7921a49147db06dd0ce4000a8a91

Queue rows bindings cleanup & small readability improvement
- Songs and stoppers now use the same bindings
- The loop for determining row selections now uses
  a pattern to access the index instead of the
  first tuple element in the loop body

src/ui/queue_page/imp.rs
@@ -595,17 +595,6 @@ impl QueuePage {
                         queue_row.add_css_class("card");
                     }
 
-                    queue_row.add_bindings(&[
-                        queue_item_object
-                            .bind_property("artwork", &row_imp.prefix_image.get(), "paintable")
-                            .sync_create()
-                            .build(),
-                        queue_item_object
-                            .bind_property("selected", &row_imp.selection_toggle.get(), "active")
-                            .sync_create()
-                            .build(),
-                    ]);
-
                     let artwork = queue_item_object.artwork();
                     if artwork.is_some() {
                         queue_row.set_prefix_image(artwork.as_ref());
@@ -620,22 +609,26 @@ impl QueuePage {
                     queue_row.add_css_class("heading");
                     queue_row.add_css_class("dimmed");
 
-                    // IDEA: Draw a pause icon in place of the album cover
-
-                    queue_row.add_binding(
-                        queue_item_object
-                            .bind_property("selected", &row_imp.selection_toggle.get(), "active")
-                            .sync_create()
-                            .build(),
-                    );
+                    // IDEA: A pause icon could be shown in place of the album cover
                 }
             }
+
+            queue_row.add_bindings(&[
+                queue_item_object
+                    .bind_property("artwork", &row_imp.prefix_image.get(), "paintable")
+                    .sync_create()
+                    .build(),
+                queue_item_object
+                    .bind_property("selected", &row_imp.selection_toggle.get(), "active")
+                    .sync_create()
+                    .build(),
+            ]);
 
             let queue_index = queue_item_object.index();
             let selection_mode = match selections.borrow().as_deref() {
                 Some(selections) => {
-                    for index in selections {
-                        if index.0 == queue_index {
+                    for (index, _) in selections {
+                        if *index == queue_index {
                             queue_item_object.set_selected(true);
                         }
                     }

commit d1c1a533ba74bb5f2f50b69966662d526949088a

Fixed song queue fallback artwork not working (regression introduced in d6fc74f)
Because the row artwork is set directly on the
widget, bindings should be added before it is set,
otherwise fallback artworks get overwritten with
`None`

src/ui/queue_page/imp.rs
@@ -588,6 +588,17 @@ impl QueuePage {
             queue_row.set_title(&queue_item_object.title());
             queue_row.set_subtitle(&queue_item_object.subtitle());
 
+            queue_row.add_bindings(&[
+                queue_item_object
+                    .bind_property("artwork", &row_imp.prefix_image.get(), "paintable")
+                    .sync_create()
+                    .build(),
+                queue_item_object
+                    .bind_property("selected", &row_imp.selection_toggle.get(), "active")
+                    .sync_create()
+                    .build(),
+            ]);
+
             match queue_item_object.queue_item() {
                 QueueItem::Song(_) => {
                     if queue_item_object.playing() {
@@ -612,17 +623,6 @@ impl QueuePage {
                     // IDEA: A pause icon could be shown in place of the album cover
                 }
             }
-
-            queue_row.add_bindings(&[
-                queue_item_object
-                    .bind_property("artwork", &row_imp.prefix_image.get(), "paintable")
-                    .sync_create()
-                    .build(),
-                queue_item_object
-                    .bind_property("selected", &row_imp.selection_toggle.get(), "active")
-                    .sync_create()
-                    .build(),
-            ]);
 
             let queue_index = queue_item_object.index();
             let selection_mode = match selections.borrow().as_deref() {

src/ui/queue_subpage/mod.rs
@@ -84,6 +84,7 @@ impl QueueSubpage {
 
     /// Returns a referecne to the currently assigned `QueueItem`
     #[inline]
+    #[must_use]
     pub fn item(&self) -> Ref<'_, QueueItem> {
         self.imp().queue_item.borrow()
     }