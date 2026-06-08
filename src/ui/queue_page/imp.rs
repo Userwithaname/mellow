Userwithaname/mellow

commit 37fe021b108e15f0ac133cce755c21c814c65296

Fixed off-by-one when reordering items past start/end in repeat mode on long queues
Since this behaves slightly differently on short
queues (where the same items are shown multiple
times when repeat mode is enabled), the two cases
are now handled separately

src/ui/queue_page/imp.rs
@@ -907,16 +907,25 @@ impl QueuePage {
                 let Some(to) = list_box.row_at_y(end_y as i32).map(|row| row.index()) else {
                     return;
                 };
-                let to_index = queue_page.model_index_to_queue(to as usize);
 
-                (player_tx().send(PlayerRequest::Reorder {
+                let queue_length = queue_page.queue_length.get();
+                let short_queue = queue_length <= NUM_ITEMS_BEHIND + NUM_ITEMS_AHEAD;
+                let offset = match short_queue {
+                    false => to - from,
+                    true => {
+                        // Short queue reordering is handled differently to fix an
+                        // off-by-one issue when reordering repeat-mode wrapped items
+                        let to_index = queue_page.model_index_to_queue(to as usize);
+                        to_index as i32 - from_index as i32
+                    }
+                };
+                let _ = player_tx().send(PlayerRequest::Shift {
                     from: from_index,
-                    to: to_index,
-                }))
-                .expect(EXP_RX);
+                    by: offset as isize,
+                });
 
                 // Short queues don't need to be offset, even if wrapped items are shown
-                if queue_page.queue_length.get() <= NUM_ITEMS_BEHIND + NUM_ITEMS_AHEAD {
+                if short_queue {
                     return;
                 }
 
@@ -929,7 +938,7 @@ impl QueuePage {
                         false if from < playing && to > playing => 1,
                         true if from > playing && to <= playing => -1,
                         true if from < playing && to >= playing => 1,
-                        true if from == playing => from_index as i32 - to_index as i32,
+                        true if from == playing => offset,
                         _ => 0,
                     },
                 ));

commit 5eb36c2b6df86be92a3fb9efcd9d2ba8d457aea8

Fixed incorrect scroll offset when dragging the playing item (regression from 37fe021) and renamed `offset` to `shift_by`

src/ui/queue_page/imp.rs
@@ -910,7 +910,7 @@ impl QueuePage {
 
                 let queue_length = queue_page.queue_length.get();
                 let short_queue = queue_length <= NUM_ITEMS_BEHIND + NUM_ITEMS_AHEAD;
-                let offset = match short_queue {
+                let shift_by = match short_queue {
                     false => to - from,
                     true => {
                         // Short queue reordering is handled differently to fix an
@@ -921,7 +921,7 @@ impl QueuePage {
                 };
                 let _ = player_tx().send(PlayerRequest::Shift {
                     from: from_index,
-                    by: offset as isize,
+                    by: shift_by as isize,
                 });
 
                 // Short queues don't need to be offset, even if wrapped items are shown
@@ -938,7 +938,7 @@ impl QueuePage {
                         false if from < playing && to > playing => 1,
                         true if from > playing && to <= playing => -1,
                         true if from < playing && to >= playing => 1,
-                        true if from == playing => offset,
+                        true if from == playing => -shift_by,
                         _ => 0,
                     },
                 ));