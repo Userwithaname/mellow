Userwithaname/mellow

commit bb1c995c5d25f27fdf6a50708672bc1f03fd98ea

Added a `Forever` type so values which live for the entire runtime of the program can be accessed as `'static` references directly, without reference counting

src/ui/queue_page/imp.rs
@@ -11,7 +11,7 @@ use crate::player::{PlayerRequest, QueueItem, player_tx};
 use crate::ui::queue_page::QueueScrollAction;
 use crate::ui::{ListRow, QueueItemObject, QueueSubpage};
 use crate::ui::{UpdateUI, fallback_song_image, ui_tx};
-use crate::util::wrap_index;
+use crate::util::{Forever, wrap_index};
 
 const NUM_ITEMS_AHEAD: usize = 45;
 const NUM_ITEMS_BEHIND: usize = 45;
@@ -38,7 +38,7 @@ pub struct QueuePage {
     #[template_child]
     pub remove_selection: TemplateChild<gtk::Button>,
 
-    pub selections: Rc<RefCell<Option<Selections>>>,
+    pub selections: Forever<RefCell<Option<Selections>>>,
 
     #[template_child]
     list_box: TemplateChild<gtk::ListBox>,
@@ -588,7 +588,7 @@ impl QueuePage {
     #[inline]
     fn setup_model(&self) {
         let model = gio::ListStore::new::<QueueItemObject>();
-        let selections = Rc::clone(&self.selections);
+        let selections = self.selections.static_ref();
         let fallback_image = fallback_song_image();
         let queue_page = self.to_owned();
 

src/ui/albums_page/imp.rs
@@ -5,7 +5,6 @@ use core::sync::atomic::Ordering;
 use fastrand;
 use gtk::CompositeTemplate;
 use gtk::{gdk, gio, glib};
-use std::rc::Rc;
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 
@@ -16,7 +15,7 @@ use crate::library::{Albums, ToQueue, ToShuffledQueue};
 use crate::player::{PlayerRequest, player_tx};
 use crate::ui::{AlbumObject, FilterMode, ItemTile, LibraryFilters, LibrarySort, LibrarySortMode};
 use crate::ui::{UpdateUI, fallback_album_image, ui_tx};
-use crate::util::search;
+use crate::util::{Forever, search};
 
 #[derive(Default, CompositeTemplate)]
 #[template(file = "albums_page.ui")]
@@ -61,15 +60,15 @@ pub struct AlbumsPage {
 
     #[template_child]
     pub search_entry: TemplateChild<gtk::SearchEntry>,
-    search_query: Rc<RefCell<String>>,
+    search_query: Forever<RefCell<String>>,
 
     contents_id: Cell<u8>,
     albums: RefCell<Vec<AlbumObject>>,
     filter: RefCell<gtk::CustomFilter>,
     sorter: RefCell<gtk::CustomSorter>,
 
-    sort_mode: Rc<RefCell<LibrarySort>>,
-    album_filters: Rc<RefCell<LibraryFilters>>,
+    sort_mode: Forever<RefCell<LibrarySort>>,
+    album_filters: Forever<RefCell<LibraryFilters>>,
 
     shuffle: Cell<bool>,
     pending_scroll_pos: Cell<Option<f64>>,
@@ -310,9 +309,9 @@ impl AlbumsPage {
 
         self.albums.replace(album_objects);
 
-        let query = Rc::clone(&self.search_query);
-        let album_filters = Rc::clone(&self.album_filters);
-        let filter = gtk::CustomFilter::new(move |object| {
+        let query = self.search_query.static_ref();
+        let album_filters = self.album_filters.static_ref();
+        let filter = gtk::CustomFilter::new(|object| {
             let album_object = object.downcast_ref::<AlbumObject>().unwrap();
             let query = &query.borrow().to_lowercase();
             let score = search::query_score(query, &album_object.album().to_lowercase())
@@ -323,8 +322,8 @@ impl AlbumsPage {
         let filter_model = gtk::FilterListModel::new(Some(model), Some(filter.clone()));
         self.filter.replace(filter);
 
-        let sort_mode = Rc::clone(&self.sort_mode);
-        let sorter = gtk::CustomSorter::new(move |object_a, object_b| {
+        let sort_mode = self.sort_mode.static_ref();
+        let sorter = gtk::CustomSorter::new(|object_a, object_b| {
             let album_a = object_a.downcast_ref::<AlbumObject>().unwrap();
             let album_b = object_b.downcast_ref::<AlbumObject>().unwrap();
             album_a.order_cmp(album_b, &sort_mode.borrow())

src/ui/artists_page/imp.rs
@@ -4,7 +4,6 @@ use core::cmp;
 use fastrand;
 use gtk::CompositeTemplate;
 use gtk::{gio, glib};
-use std::rc::Rc;
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 
@@ -15,7 +14,7 @@ use crate::library::{Artists, ToQueue, ToShuffledQueue};
 use crate::player::{PlayerRequest, player_tx};
 use crate::ui::{ArtistObject, FilterMode, ItemTile, LibraryFilters, LibrarySort, LibrarySortMode};
 use crate::ui::{UpdateUI, ui_tx};
-use crate::util::search;
+use crate::util::{Forever, search};
 
 #[derive(Default, CompositeTemplate)]
 #[template(file = "artists_page.ui")]
@@ -53,15 +52,15 @@ pub struct ArtistsPage {
 
     #[template_child]
     pub search_entry: TemplateChild<gtk::SearchEntry>,
-    search_query: Rc<RefCell<String>>,
+    search_query: Forever<RefCell<String>>,
 
     contents_id: Cell<u8>,
     artists: RefCell<Vec<ArtistObject>>,
     filter: RefCell<gtk::CustomFilter>,
     sorter: RefCell<gtk::CustomSorter>,
 
-    sort_mode: Rc<RefCell<LibrarySort>>,
-    artist_filters: Rc<RefCell<LibraryFilters>>,
+    sort_mode: Forever<RefCell<LibrarySort>>,
+    artist_filters: Forever<RefCell<LibraryFilters>>,
 
     shuffle: Cell<bool>,
     pending_scroll_pos: Cell<Option<f64>>,
@@ -290,8 +289,8 @@ impl ArtistsPage {
 
         self.artists.replace(artist_objects);
 
-        let query = Rc::clone(&self.search_query);
-        let artist_filters = Rc::clone(&self.artist_filters);
+        let query = self.search_query.static_ref();
+        let artist_filters = self.artist_filters.static_ref();
         let filter = gtk::CustomFilter::new(move |object| {
             let artist_object = object.downcast_ref::<ArtistObject>().unwrap();
             let score = search::query_score(
@@ -304,7 +303,7 @@ impl ArtistsPage {
         let filter_model = gtk::FilterListModel::new(Some(model), Some(filter.clone()));
         self.filter.replace(filter);
 
-        let sort_mode = Rc::clone(&self.sort_mode);
+        let sort_mode = self.sort_mode.static_ref();
         let sorter = gtk::CustomSorter::new(move |object_a, object_b| {
             let artist_a = object_a.downcast_ref::<ArtistObject>().unwrap();
             let artist_b = object_b.downcast_ref::<ArtistObject>().unwrap();

src/ui/songs_page/imp.rs
@@ -5,7 +5,6 @@ use core::sync::atomic::Ordering;
 use fastrand;
 use gtk::CompositeTemplate;
 use gtk::{gdk, gio, glib};
-use std::rc::Rc;
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 
@@ -16,7 +15,7 @@ use crate::library::{Songs, ToQueue};
 use crate::player::{PlayerRequest, player_tx};
 use crate::ui::{FilterMode, ItemRow, LibraryFilters, LibrarySort, LibrarySortMode, SongObject};
 use crate::ui::{UpdateUI, fallback_song_image, ui_tx};
-use crate::util::search;
+use crate::util::{Forever, search};
 
 #[derive(Default, CompositeTemplate)]
 #[template(file = "songs_page.ui")]
@@ -61,15 +60,15 @@ pub struct SongsPage {
 
     #[template_child]
     pub search_entry: TemplateChild<gtk::SearchEntry>,
-    search_query: Rc<RefCell<String>>,
+    search_query: Forever<RefCell<String>>,
 
     contents_id: Cell<u8>,
     songs: RefCell<Vec<SongObject>>,
     filter: RefCell<gtk::CustomFilter>,
     sorter: RefCell<gtk::CustomSorter>,
 
-    sort_mode: Rc<RefCell<LibrarySort>>,
-    song_filters: Rc<RefCell<LibraryFilters>>,
+    sort_mode: Forever<RefCell<LibrarySort>>,
+    song_filters: Forever<RefCell<LibraryFilters>>,
 
     shuffle: Cell<bool>,
     pending_scroll_pos: Cell<Option<f64>>,
@@ -306,9 +305,9 @@ impl SongsPage {
 
         self.songs.replace(song_objects);
 
-        let query = Rc::clone(&self.search_query);
-        let song_filters = Rc::clone(&self.song_filters);
-        let filter = gtk::CustomFilter::new(move |object| {
+        let query = self.search_query.static_ref();
+        let song_filters = self.song_filters.static_ref();
+        let filter = gtk::CustomFilter::new(|object| {
             let song_object = object.downcast_ref::<SongObject>().unwrap();
             let query = &query.borrow().to_lowercase();
             let score = search::query_score(query, &song_object.song().to_lowercase())
@@ -319,8 +318,8 @@ impl SongsPage {
         let filter_model = gtk::FilterListModel::new(Some(model), Some(filter.clone()));
         self.filter.replace(filter);
 
-        let sort_mode = Rc::clone(&self.sort_mode);
-        let sorter = gtk::CustomSorter::new(move |object_a, object_b| {
+        let sort_mode = self.sort_mode.static_ref();
+        let sorter = gtk::CustomSorter::new(|object_a, object_b| {
             let song_a = object_a.downcast_ref::<SongObject>().unwrap();
             let song_b = object_b.downcast_ref::<SongObject>().unwrap();
             song_a.order_cmp(song_b, &sort_mode.borrow())

src/ui/window/imp.rs
@@ -4,7 +4,6 @@ use core::cell::{Cell, OnceCell, RefCell};
 use core::time::Duration;
 use glib::subclass::InitializingObject;
 use gtk::{CompositeTemplate, gio, glib};
-use std::rc::Rc;
 use std::sync::Arc;
 use std::thread;
 
@@ -20,6 +19,7 @@ use crate::ui::{
 use crate::ui::{LibraryPage, LyricsPage, MainPlayer, SettingsPage, SubpageType};
 use crate::ui::{QueuePage, QueueSubpage};
 use crate::ui::{UpdateUI, ui_tx};
+use crate::util::Forever;
 
 #[derive(Default, CompositeTemplate)]
 #[template(file = "window.ui")]
@@ -50,10 +50,10 @@ pub struct Window {
     #[template_child]
     pub artists_page: TemplateChild<ArtistsPage>,
 
-    pub library_subpages: Rc<RefCell<Vec<SubpageType>>>,
-    pub song_pages: Rc<RefCell<Vec<SongPage>>>,
-    pub album_pages: Rc<RefCell<Vec<AlbumPage>>>,
-    pub artist_pages: Rc<RefCell<Vec<ArtistPage>>>,
+    pub library_subpages: RefCell<Vec<SubpageType>>,
+    pub song_pages: RefCell<Vec<SongPage>>,
+    pub album_pages: Forever<RefCell<Vec<AlbumPage>>>,
+    pub artist_pages: Forever<RefCell<Vec<ArtistPage>>>,
 
     // View stack "Playing" tab
     #[template_child]

src/library/mod.rs
@@ -25,6 +25,7 @@ use crate::library::tag_list::Taggable;
 use crate::library::{album::NewSharedAlbum, artist::NewSharedArtist};
 use crate::player::{PlayerRequest, QueueItem, player_tx};
 use crate::ui::{UpdateUI, ui_tx};
+use crate::util::Forever;
 use crate::util::hint::likely;
 use crate::util::tasks::{BoxedTask, Runner};
 use crate::{songs_file, util::visit_dirs};
@@ -45,7 +46,7 @@ pub struct Library {
     artists: Artists,
 
     missing_songs: Songs,
-    check_moved: Arc<Mutex<Songs>>,
+    check_moved: Forever<Mutex<Songs>>,
     undo_songs: Songs,
 
     rebuild_pending: bool,
@@ -284,7 +285,7 @@ impl Library {
             artists: Vec::new(),
 
             missing_songs: Vec::new(),
-            check_moved: Arc::new(Mutex::new(Vec::new())),
+            check_moved: Forever::new(Mutex::new(Vec::new())),
             undo_songs: Vec::new(),
 
             rebuild_pending: false,
@@ -394,10 +395,10 @@ impl Library {
         self.tasks.run({
             let songs = self.songs.clone();
             let missing = mem::take(&mut self.missing_songs);
-            let check = Arc::clone(&self.check_moved);
+            let check = self.check_moved.static_ref();
             let config = self.config.clone();
             let n_workers = self.tasks.num_workers();
-            move || match Library::create_connections(songs, missing, &check, &config, n_workers) {
+            move || match Library::create_connections(songs, missing, check, &config, n_workers) {
                 Ok(()) => (),
                 Err(e) => eprintln!("`Library::create_connections`: {e}"),
             }
@@ -417,7 +418,7 @@ impl Library {
     fn create_connections(
         mut songs: Songs,
         mut missing: Songs,
-        check_moved: &Arc<Mutex<Songs>>,
+        check_moved: &Mutex<Songs>,
         config: &LibraryConfig,
         num_workers: usize,
     ) -> Result<(), Box<dyn Error>> {
@@ -691,7 +692,7 @@ impl Library {
     fn validate_songs(
         songs: &mut Songs,
         missing: &mut Songs,
-        check_moved: &Arc<Mutex<Songs>>,
+        check_moved: &Mutex<Songs>,
         config: &LibraryConfig,
     ) {
         let mut libraries = Vec::with_capacity(config.directories().len());

src/ui/actions/menu.rs
@@ -2,7 +2,6 @@ use core::cell::RefCell;
 use gio::prelude::*;
 use glib::{GString, VariantTy};
 use gtk::{gio, glib};
-use std::rc::Rc;
 
 use crate::ui::SongsPage;
 use crate::ui::{AlbumPage, AlbumsPage};
@@ -114,12 +113,12 @@ pub fn songs_play_mode(songs_page: SongsPage) -> gio::ActionEntry<gio::SimpleAct
 
 #[inline]
 pub fn artist_page_play_mode(
-    artist_pages: Rc<RefCell<Vec<ArtistPage>>>,
+    artist_pages: &'static RefCell<Vec<ArtistPage>>,
 ) -> gio::ActionEntry<gio::SimpleActionGroup> {
     gio::ActionEntry::builder("artist_page_play_mode")
         .parameter_type(Some(VariantTy::STRING))
         .state("Sequential".to_variant())
-        .activate(move |_, action, variant| {
+        .activate(|_, action, variant| {
             let variant = variant.unwrap();
             action.set_state(variant);
             artist_pages.borrow().last().inspect(|album_page| {
@@ -134,12 +133,12 @@ pub fn artist_page_play_mode(
 }
 #[inline]
 pub fn album_page_play_mode(
-    album_pages: Rc<RefCell<Vec<AlbumPage>>>,
+    album_pages: &'static RefCell<Vec<AlbumPage>>,
 ) -> gio::ActionEntry<gio::SimpleActionGroup> {
     gio::ActionEntry::builder("album_page_play_mode")
         .parameter_type(Some(VariantTy::STRING))
         .state("Sequential".to_variant())
-        .activate(move |_, action, variant| {
+        .activate(|_, action, variant| {
             let variant = variant.unwrap();
             action.set_state(variant);
             album_pages.borrow().last().inspect(|album_page| {

src/ui/actions/mod.rs
@@ -1,7 +1,6 @@
 use adw::{prelude::*, subclass::prelude::*};
 use glib::GString;
 use gtk::{gio, glib};
-use std::rc::Rc;
 
 use crate::about::show_about_dialog;
 use crate::shortcuts::show_shortcuts_dialog;
@@ -52,8 +51,8 @@ impl WindowActions for Window {
                 actions::player::play_all_songs(&window.songs_page),
                 actions::player::play_all_albums(&window.albums_page),
                 actions::player::play_all_artists(&window.artists_page),
-                actions::player::queue_visible_album(Rc::clone(&window.album_pages)),
-                actions::player::queue_visible_artist(Rc::clone(&window.artist_pages)),
+                actions::player::queue_visible_album(window.album_pages.static_ref()),
+                actions::player::queue_visible_artist(window.artist_pages.static_ref()),
                 actions::player::refresh_library(),
             ]
         });
@@ -82,8 +81,8 @@ impl WindowActions for Window {
             actions::menu::songs_play_mode(window.songs_page.get()),
             actions::menu::albums_play_mode(window.albums_page.get()),
             actions::menu::artists_play_mode(window.artists_page.get()),
-            actions::menu::album_page_play_mode(Rc::clone(&window.album_pages)),
-            actions::menu::artist_page_play_mode(Rc::clone(&window.artist_pages)),
+            actions::menu::album_page_play_mode(window.album_pages.static_ref()),
+            actions::menu::artist_page_play_mode(window.artist_pages.static_ref()),
         ]);
         self.insert_action_group("menu", Some(&menu_actions));
     }

commit 601e76bed85e9b69f055046b3bb1bf667f5f1edc

Fixed issues entering selection mode (regression from bb1c995)
`selections` is now accessed by reference rather
than through `Rc`, so it should've been removed
from `glib::clone`

src/ui/queue_page/imp.rs
@@ -651,8 +651,6 @@ impl QueuePage {
                 queue_page,
                 #[weak]
                 queue_item_object,
-                #[strong]
-                selections,
                 move |_| if let Some(selections) = &mut *selections.borrow_mut() {
                     let selected = queue_page.toggle_selected_item(
                         (